d069701ba98bfa49508304c455a8fb38bfafa721
Adrien Martins <adrien.martins@worldline.com>
2015-12-17 09:52:14 +0000

    Functionnality review
    Inferance option has been remove (because we cannot infer anything on the different response that can be returned).
    Add inferance on _content-type_ header.
    Major code refactoring in lib/test-generation.js.

--- test/test-generation.js
+++ test/test-generation.js
@@ -8,44 +8,24 @@ var spec = require('./fixtures/swagger');
 describe('test generation', function () {
   var xamples;
 
-  describe('with inference', function(){
-    before(function(){
-      xamples = testGenerator.parse(spec, { inferXamples: true });
-    });
-
-    it('should contain three test cases', function () {
-      expect(xamples.length).to.equal(3);
-    });
-
-    it('should test GET /pets first', function () {
-      expect(xamples[0]).to.eql(result.petsXample);
-    });
-
-    it ('should test GET /pets/fido4 second', function () {
-      expect(xamples[1]).to.eql(result.petsFido4Xample);
-    });
-
-    it ('should test GET /pets/fido7 last', function () {
-      expect(xamples[2]).to.eql(result.petsFido7Xample);
-    });
+  before(function(){
+    xamples = testGenerator.parse(spec, { inferXamples: false });
   });
 
-  describe('without inference', function(){
+  it('should contain three test cases', function () {
+    console.log(xamples);
+    expect(xamples.length).to.equal(3);
+  });
 
-    before(function(){
-      xamples = testGenerator.parse(spec, { inferXamples: false });
-    });
+  it ('should test GET /pets', function () {
+    expect(xamples[0]).to.eql(result.petsXample);
+  });
 
-    it('should contain three test cases', function () {
-      expect(xamples.length).to.equal(2);
-    });
+  it ('should test GET /pets/fido4', function () {
+    expect(xamples[1]).to.eql(result.petsFido4Xample);
+  });
 
-    it ('should test GET /pets/fido4 first', function () {
-      expect(xamples[0]).to.eql(result.petsFido4Xample);
-    });
-
-    it ('should test GET /pets/fido7 last', function () {
-      expect(xamples[1]).to.eql(result.petsFido7Xample);
-    });
+  it ('should test GET /pets/fido7', function () {
+    expect(xamples[2]).to.eql(result.petsFido7Xample);
   });
 });
